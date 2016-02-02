f8c8c8d6c2afbdc0647260baf6438c9db79c3055
Jarmo Luostarinen <k3295@student.jamk.fi>
2016-02-02 19:43:42 +0200

    jeba

---
+++ Demo7/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = null;
+            double d;
+            int i;
+            {
+                // kysytään käyttäjältä rivi
+                StreamWriter outputFile = new StreamWriter("integer.txt");
+                StreamWriter outputFile2 = new StreamWriter("double.txt");
+                do
+                {
+
+                    Console.WriteLine("Give a number (enter stops):");
+                    number = Console.ReadLine();
+
+
+                    bool parsed = int.TryParse(number, out i);
+                    if (!parsed)
+                    {
+                        double.TryParse(number, out d);
+                        outputFile2.WriteLine(d); // kirjoitetaan levylle
+                    }
+                    else
+                    {
+                        outputFile.WriteLine(i);
+
+
+
+                    }
+
+                } while (number.Length != 0);
+
+
+
+                // suljetaan tiedosto
+                outputFile.Close();
+                outputFile2.Close();
+
+                try
+                {
+                    using (StreamReader sr = new StreamReader("integer.txt"))
+                    using (StreamReader sr1 = new StreamReader("double.txt"))
+                    {
+                        // read the stream to a string, and write the string to console
+                        String line2 = sr.ReadToEnd();
+                        String line3 = sr1.ReadToEnd();
+                        Console.WriteLine(line2);
+                        Console.WriteLine(line3);
+
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("There werw some problems writing or reading the file:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
